4c92412bba3f42932e715a58cc36df01f47aed2d
lantengtenghuaqiu <m13953332167@163.com>
2026-03-03 09:00:28 +0800

    Shadow

---
+++ D_OpenGL/A_3_Shadow/LocationManger.hpp
@@ -0,0 +1,8 @@
+struct Loaction_CubeMap{
+    unsigned int _skybox;
+};
+
+struct Location_Container
+{
+    unsigned int _M;
+};
--- D_OpenGL/A_3_Shadow/main.cpp
+++ D_OpenGL/A_3_Shadow/main.cpp
@@ -1,70 +1,133 @@
+// clang -c ../src/glad.c  -I ../includes -o ./builder/glad.o &&
+// clang++ -c main.cpp -I ../includes -std=c++17 -o ./builder/main.o &&
+// clang++ ./builder/main.o ./builder/glad.o -L ../libs/mac -lglfw -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -std=c++17  -o main
+
+// g++ main.cpp ..\src\glad.c -I ..\includes -L ..\libs -lopengl32 -lglfw3 -lgdi32 -lassimp -lz -fno-permissive -Wno-unused-variable -Wno-unused-parameter  -Wall -Wextra -std=c++17 -o main.exe
+// #define FILE_DEBUG
+// #define TRANSFORM_DEBUG
+
+#include <chrono>
 #include "../includes/Window.hpp"
 #include "../includes/Event.hpp"
+#include "LocationManger.hpp"
 
 void glClearConfig();
 void ObjectIndexManger(ObjectIndex *OID);
+void BindShader(GLAD &glad, Tools::TheFile *&file, Shader &shader, const char *path_vertex, const char *path_fragment);
+
+float speed = 0.0f;
 
 int main()
 {
     GLFW glfw;
-    GLAD<float> glad;
+    GLAD glad;
     if (Init_GLFW_GLAD(glfw, glad))
     {
+        // Mangers
         ObjectIndex *OID = ObjectIndex::GetIntance();
         ObjectIndexManger(OID);
-
-        Picture::ImageManager *picture = Picture::ImageManager::GetInstance();
-        ReadFile::TheFile *file = ReadFile::TheFile::GetInstance();
-
-        Vertices vertex;
+        Tools::ImageManager *image = Tools::ImageManager::GetInstance();
+        Tools::TheFile *file = Tools::TheFile::GetInstance();
+        // Vertices vertex;
 
         Hierarchy hierarchy;
 
-        // Container
+        // Camera--------------------------------------------------------------
+        hierarchy.sceneObjects.emplace("Camera", Camera());
+        Camera *HierarchyPointer_camera = &std::get<Camera>(hierarchy.sceneObjects["Camera"]);
+        {
+            SetCamera(HierarchyPointer_camera, 10.0f, 10.0f);
+        }
+        BindTransformUniformBufferObject(OID, HierarchyPointer_camera);
+
+        // Cube Map Box------------------------------------
+        hierarchy.sceneObjects.emplace("CubeMap", SceneObject());
+        SceneObject *HierarchyPointer_cubmap = &std::get<SceneObject>(hierarchy.sceneObjects["CubeMap"]);
+        {
+        }
+        BindSceneObject<float>(OID, Cube::mesh, Cube::normal, Cube::uv);
+
+        BindCubeMapTexture(OID, image);
+
+        Shader Shader_cubeMap;
+        BindShader(glad, file, Shader_cubeMap, "/D_OpenGL/Assets/Materials/CubeMap.vertex", "/D_OpenGL/Assets/Materials/CubeMap.fragment");
+
+        Loaction_CubeMap location_cubmap;
+        location_cubmap._skybox = glGetUniformLocation(Shader_cubeMap.Program, "skybox");
+
+        // Container---------------------------------------
         hierarchy.sceneObjects.emplace("Cube", SceneObject());
-        SceneObject *Pointer_object_cube = &std::get<SceneObject>(hierarchy.sceneObjects["Cube"]);
+        SceneObject *HierarchyPointer_cube = &std::get<SceneObject>(hierarchy.sceneObjects["Cube"]);
 
-        glBindVertexArray(OID->VAO[OID->GetCID_VAO('w')]);
-        glBindBuffer(GL_ARRAY_BUFFER, OID->VBO[OID->GetCID_VBO('w')]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(Cube::mesh) + sizeof(Cube::normal) + sizeof(Cube::uv), NULL, GL_STATIC_DRAW);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Cube::mesh), Cube::mesh);
-        glBufferSubData(GL_ARRAY_BUFFER, sizeof(Cube::mesh), sizeof(Cube::normal), Cube::normal);
-        glBufferSubData(GL_ARRAY_BUFFER, sizeof(Cube::mesh) + sizeof(Cube::normal), sizeof(Cube::uv), Cube::uv);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)0);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void *)sizeof(Cube::mesh));
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (void *)sizeof(Cube::mesh) + sizeof(Cube::normal));
-        glEnableVertexAttribArray(2);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
+        BindSceneObject<float>(OID, Cube::mesh, Cube::normal, Cube::uv);
 
         Shader Shader_container;
-
-        glad.GetShadersData(file, "/D_OpenGL/A_3_Shadow/box.vertex", Shader_container.ShaderData);
-        glad.CompileAndAttachShader(Shader_container.Program, Shader_container.Vertex, Shader_container.ShaderData, "Vertex");
-
-        glad.GetShadersData(file, "/D_OpenGL/A_3_Shadow/box.fragment", Shader_container.ShaderData);
-        glad.CompileAndAttachShader(Shader_container.Program, Shader_container.Fragment, Shader_container.ShaderData, "Fragment");
-        glad.LinkShaderProgram(Shader_container.Program);
-
-        glad.DetachAndDeleteShaders(Shader_container);
+        BindShader(glad, file, Shader_container, "/D_OpenGL/A_3_Shadow/Shaders/box.vertex", "/D_OpenGL/A_3_Shadow/Shaders/box.fragment");
+        // Loactions
+        Location_Container location_container;
+        location_container._M = glGetUniformLocation(Shader_container.Program, "M");
 
         // --------------------------------------------------------------------
         // Clear config
         glClearConfig();
-
-        // Frame flash
+        //---------------------------------------------------------------------
+        auto first_frame_time = std::chrono::high_resolution_clock::now();
+        auto last_frame_time = std::chrono::high_resolution_clock::now();
+        auto current_frame_time = std::chrono::high_resolution_clock::now();
+        float deltTime = 0;
+        float time = 0;
+        // Main While-----------------------------------------------------------
         while (!glfwWindowShouldClose(glfw.window))
         {
+            // 帧速率:
+            current_frame_time = std::chrono::high_resolution_clock::now();
+            deltTime = std::chrono::duration_cast<std::chrono::duration<float>>(current_frame_time - last_frame_time).count();
+            last_frame_time = std::chrono::high_resolution_clock::now();
+            time = std::chrono::duration_cast<std::chrono::duration<float>>(current_frame_time - first_frame_time).count();
+            speed = deltTime;
+
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-            glUseProgram(Shader_container.Program);
+            // Draw Calls
+            glEnable(GL_CULL_FACE);
+            // Draw the cube map box
+            glCullFace(GL_FRONT);
+            glDepthMask(GL_FALSE);
+            glUseProgram(Shader_cubeMap.Program);
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_CUBE_MAP, OID->TEX[0]);
+            glUniform1i(location_cubmap._skybox, 0);
+
+            if (Shader_cubeMap.init == false)
+            {
+
+                Shader_cubeMap.init = true;
+            }
 
             glBindVertexArray(OID->VAO[0]);
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+            glBindVertexArray(0);
+            glDepthMask(GL_TRUE);
+            glCullFace(GL_BACK);
+
+            // Draw a container------------------------------------------------
+            glUseProgram(Shader_container.Program);
+
+            // Key Board Event-----------------------------
+            // KeyRotate(glfw.window, *HierarchyPointer_cube, speed * 35.0f, HierarchyPointer_cube->Rotation._mat4);
+            // KeyTranslate(glfw.window, *HierarchyPointer_cube, speed * 20.0f, HierarchyPointer_cube->Translate._mat4);
+
+            if (Shader_container.init == false)
+            {
+                Transform::TranslateZ(5.0f, HierarchyPointer_cube->Translate._mat4);
+                Shader_container.init = true;
+            }
+
+            HierarchyPointer_cube->UpdataModelMatrix();
+            glUniformMatrix4fv(location_container._M, 1, GL_FALSE, HierarchyPointer_cube->Transform._mat4);
+
+            glBindVertexArray(OID->VAO[1]);
             glDrawArrays(GL_TRIANGLES, 0, 36);
             glBindVertexArray(0);
 
@@ -84,10 +147,10 @@ void glClearConfig()
 
 void ObjectIndexManger(ObjectIndex *OID)
 {
-    OID->CreateAndGenObjectIndex(OID_VAO, 1);
+    OID->CreateAndGenObjectIndex(OID_VAO, 2);
     glGenVertexArrays(OID->sizeVAO, OID->VAO);
 
-    OID->CreateAndGenObjectIndex(OID_VBO, 1);
+    OID->CreateAndGenObjectIndex(OID_VBO, 2);
     glGenBuffers(OID->sizeVBO, OID->VBO);
 
     OID->CreateAndGenObjectIndex(OID_TEX, 1);
@@ -100,4 +163,25 @@ void ObjectIndexManger(ObjectIndex *OID)
     glGenBuffers(OID->sizeUBO, OID->UBO);
 
     OID->CreateAndGenObjectIndex(OID_FBO, 1);
+}
+
+void BindShader(GLAD &glad, Tools::TheFile *&file, Shader &shader, const char *path_vertex, const char *path_fragment)
+{
+#ifdef __APPLE__
+    glad.GetShadersData(file, path_vertex, shader.ShaderData);
+    glad.CompileAndAttachShader(shader.Program, shader.Vertex, shader.ShaderData, "Vertex");
+
+    glad.GetShadersData(file, path_fragment, shader.ShaderData);
+    glad.CompileAndAttachShader(shader.Program, shader.Fragment, shader.ShaderData, "Fragment");
+#else
+    glad.GetShadersData(file, path_vertex, shader.ShaderData);
+    glad.CompileAndAttachShader(shader.Program, shader.Vertex, shader.ShaderData, "Vertex");
+
+    glad.GetShadersData(file, path_fragment, shader.ShaderData);
+    glad.CompileAndAttachShader(shader.Program, shader.Fragment, shader.ShaderData, "Fragment");
+#endif
+
+    glad.LinkShaderProgram(shader.Program);
+
+    glad.DetachAndDeleteShaders(shader);
 }
